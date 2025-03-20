90d9c3f678ddce9cf8bab59b22acd6996a7ecc98
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-03-20 16:06:48 +0100

    added code example to readme creation

--- R/archive_init.R
+++ R/archive_init.R
@@ -5,27 +5,26 @@
 archive_init <- function(archive_name,
                          parent_dir = NULL,
                          remote_owner = "opentsi",
-                         rproj = TRUE
-
-){
-  if(is.null(parent_dir)){
-    archive_path = file.path(getwd(), archive_name)
+                         rproj = TRUE) {
+  if (is.null(parent_dir)) {
+    archive_path <- file.path(getwd(), archive_name)
   } else {
-    archive_path = file.path(parent_dir, archive_name)
+    archive_path <- file.path(parent_dir, archive_name)
   }
 
-  generic_desc <- sprintf("The %s package provides versioned time series data
+  generic_desc <- sprintf(
+    "The %s package provides versioned time series data
                             and their meta information for scientific research.
                             In addition, the package contains the
                             extract-transform-load (ETL) functionality that
                             sources the data from its original provider.",
-                          archive_name
-                          )
-  generic_desc_sw <- gsub(" +", " ",  generic_desc)
+    archive_name
+  )
+  generic_desc_sw <- gsub(" +", " ", generic_desc)
   generic_desc_no_ls <- gsub("\n\\s+", "\n", generic_desc_sw)
 
   readme_md_content <- sprintf(
-"
+    "
 # %s
 
 %s
@@ -34,13 +33,24 @@ archive_init <- function(archive_name,
 
 ## Basic Usage Via opentimeseries
 
+
+```r
+ts <- read_open_ts(
+  \"%s\",
+  archive= \"opentsi_org\" # or your organisation
+)
+
+ts
+```
+
 ## The %s Data R Package
 
 ",
-archive_name,
-generic_desc_no_ls,
-archive_name
-)
+    archive_name,
+    generic_desc_no_ls,
+    archive_name,
+    archive_name
+  )
 
   usethis::create_package(
     path = archive_path,
@@ -48,10 +58,13 @@ archive_name
     roxygen = TRUE,
     open = FALSE,
     fields = list(
-      Title = sprintf("Versioned Long Format Time Series from %s",
-                      toupper(archive_name)),
+      Title = sprintf(
+        "Versioned Long Format Time Series from %s",
+        toupper(archive_name)
+      ),
       Version = "0.1",
-      Description = generic_desc)
+      Description = generic_desc
+    )
   )
 
   readme_loc <- file.path(archive_path, "README.md")
@@ -60,7 +73,7 @@ archive_name
   writeLines(readme_md_content, file_con)
   close(file_con)
 
-  use_data_raw(name = gsub("\\.","", archive_name))
+  use_data_raw(name = gsub("\\.", "", archive_name))
   fs::file_move(path = "data-raw", file.path(archive_path, "data-raw"))
 
 
@@ -71,8 +84,8 @@ archive_name
   git_init(archive_path)
   repo
 
-   if(!is.null(remote_owner)){
-     # push to Remote
-     # see Colin's slides.
-   }
+  if (!is.null(remote_owner)) {
+    # push to Remote
+    # see Colin's slides.
+  }
 }
